b8f0762262563aa78e00054c65bb1d6b50066776
NVVPrabhanjan <nvvenkatprabhanjan@gmail.com>
2025-02-08 21:06:58 +0530

    changes

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -9,7 +9,8 @@ const isAuthenticated = async (req, res, next) => {
                 success: false,
             })
         }
-        const decode = await jwt.verify(token, process.env.SECRET_KEY);
+        const decode = await jwt.verify(token, "your_secret_key_here");
+        console.log(decode,token,process.env.SECRET_KEY)
         if(!decode){
             return res.status(401).json({
                 message:"Invalid token",
