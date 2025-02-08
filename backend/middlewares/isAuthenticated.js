d3b66205a074ff60247b35c464b069a989e6a72e
NVVPrabhanjan <nvvenkatprabhanjan@gmail.com>
2025-02-08 21:47:08 +0530

    change

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -4,6 +4,7 @@ const isAuthenticated = async (req, res, next) => {
     try {
         const token = req.cookies.token;
         console.log(token);
+        console.log(req);
         if (!token) {
             return res.status(401).json({
                 token,
