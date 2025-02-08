2e8ef8c4b57265a5052c5deb0b8f4ded65e79a13
NVVPrabhanjan <nvvenkatprabhanjan@gmail.com>
2025-02-08 21:56:16 +0530

    change

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,6 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
     origin: "https://job-portal-weld-one.vercel.app",
-    // origin:"*",
     credentials:true
 }
 
